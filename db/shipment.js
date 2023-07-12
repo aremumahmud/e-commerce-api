0dff5c11da85ca6cec740e0d4365a19408345370
mahmud aremu <aremumahmud2003@gmail.com>
2023-07-12 15:42:46 +0100

    successfully integrated the progress  view

--- db/shipment.js
+++ db/shipment.js
@@ -1,6 +1,13 @@
 const shipmentModel = require("./Models/shipment.Model");
 
 class Shipment {
+    /**
+     * The function `updateShipment` updates an existing shipment object with new local and
+     * international values, or creates a new shipment object if one does not already exist.
+     * @param shipment - The `shipment` parameter is an object that contains the details of a shipment.
+     * It may have the following properties:
+     * @returns a promise that resolves to the updated shipment object.
+     */
     updateShipment(shipment) {
         return shipmentModel.findOne().then(res => {
             if (!res) {
@@ -16,6 +23,8 @@ class Shipment {
         })
     }
 
+    /* The `getShipment()` function retrieves the details of a shipment object. It uses the
+    `shipmentModel.findOne()` method to find the first shipment object in the database. */
     getShipment() {
         return shipmentModel.findOne().then(res => {
             if (!res) {
