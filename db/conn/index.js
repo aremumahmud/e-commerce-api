6a790899f7b831b2c20cdb2134fe84695375ac1c
mahmud aremu <aremumahmud2003@gmail.com>
2023-07-15 14:17:20 +0100

    thuss is just a marker

--- db/conn/index.js
+++ db/conn/index.js
@@ -1,4 +1,4 @@
-const URI = 'mongodb+srv://aremumahmud:aremu2003@cluster0.imbxhgs.mongodb.net/?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/testdb5'
+const URI = process.env.MONGOOSE_URI || 'mongodb://127.0.0.1:27017/testdb5'
 const mongoose = require('mongoose')
 
 mongoose.connect(URI)
