a3228a7e72673dc2c0340c695eb747a5ee9124f7
mahmud aremu <aremumahmud2003@gmail.com>
2023-08-22 01:32:17 +0100

    added more states

--- tt.js
+++ tt.js
@@ -1,51 +1,45 @@
-// // let edit = new Proxy({
-// //     process: 'jn'
-// // }, {
-// //     set: (...args) => {
-// //         console.log(args)
-// //     },
+const fs = require('fs');
+const cron = require('node-cron');
+const axios = require('axios');
 
-// const categoryModel = require('./db/Models/category.Model')
+const API_KEY = '96e843faa36d4980ad0c05ff4c0f2b71'; // Replace with your API key
+const BASE_CURRENCY = 'USD';
 
-// //     get: (...args) => {
-// //         console.log(args)
-// //     }
-// // })
+// Function to fetch exchange rates and log them to a file
+async function fetchAndLogExchangeRates() {
+    try {
+        const response = await axios.get(`https://openexchangerates.org/api/latest.json?app_id=${API_KEY}&base=${BASE_CURRENCY}&symbols=NGN,GBP,EUR`);
+        const exchangeRates = response.data.rates;
+        const timestamp = new Date().toISOString();
 
-// // edit.danxe = '34'
-// // edit.danxe
+        const usdToNgnRate = exchangeRates.NGN
+        const usdToGbpRate = exchangeRates.GBP
+        const usdToEurRate = exchangeRates.EUR
 
-// require('./db/conn')
-// new categoryModel({
-//     categories: []
-// }).save().then(res => {
-//     console.log('successfully create the category element')
-// }).catch(err => {
-//     console.log('failed with error', err)
-// })
+        const gbpToNgnRate = usdToNgnRate / usdToGbpRate;
+        const eurToNgnRate = usdToNgnRate / usdToEurRate;
 
-const ids = [
-    '648c48ab44d3c031bd92ea48',
-    '648c48ac44d3c031bd92ea4a',
-    '648c48ab44d3c031bd92ea48'
-];
-require('./db/conn/index')
-const mongoose = require('mongoose');
-const { modifyExchange } = require('./db/exchange');
-// const colorModel = require('./db/Models/color.Model');
-// const ObjectId = mongoose.Types.ObjectId;
+        console.log(`1 GBP in NGN: ${gbpToNgnRate}`);
+        console.log(`1 EUR in NGN: ${eurToNgnRate}`);
 
-// const objectIds = ids.map(id => new ObjectId(id));
+        const logMessage = `${timestamp}: ${BASE_CURRENCY} to USD: ${exchangeRates.NGN}, ${BASE_CURRENCY} to GBP: ${exchangeRates.GBP}, ${BASE_CURRENCY} to EUR: ${exchangeRates.EUR}\n`;
 
-// colorModel.find({ '_id': { $in: objectIds } })
-//     .then(results => {
-//         console.log(results);
-//     })
-//     .catch(err => {
-//         console.error(err);
-//     });
-modifyExchange({
-    USD: 1000
-}).then(res => {
-    console.log(res)
-})
+        fs.appendFile('exchange_rates.log', logMessage, (err) => {
+            if (err) {
+                console.error('Error writing to log file:', err);
+            } else {
+                console.log('Exchange rates logged successfully.');
+            }
+        });
+    } catch (error) {
+        console.error('Error fetching exchange rates:', error);
+    }
+}
+
+// Schedule the cron job to run every 10 hours
+cron.schedule('0 */10 * * *', () => {
+    console.log('Fetching and logging exchange rates...');
+    fetchAndLogExchangeRates();
+});
+fetchAndLogExchangeRates();
+console.log('Exchange rate cron job started.');
